b67257b40db5d514f27b77641387eb38f0ebaf53
Merveilles K <53955888+Mervhope144k@users.noreply.github.com>
2023-03-19 19:09:03 +0200

    learning c with Head First C

---
+++ Leesson_01.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    char ex[20];
+    puts("Enter boyfriend's name: ");
+    scanf("%19s", ex);
+    printf("Dear %s.\n\n\tYou're history.\n", ex);
+    return 0;
+}
