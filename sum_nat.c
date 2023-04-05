ea1e8e43a17d6942a80906293167e270f9254588
Merveilles <merveilleskatumba@gmail.com>
2023-04-05 16:28:22 +0200

    recursion in C

---
+++ sum_nat.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * sum - this function does sim of natural numbers using recursion
+ * @x: argument of the function
+ */
+
+int sum(int x)
+{
+	int s = 0;
+	if(x == 1)
+		return x;
+	s = x + sum(x - 1);
+	return s;
+}
+void main()
+{
+	int a;
+	a = sum(5);
+	printf("%d\n",a);
+}
